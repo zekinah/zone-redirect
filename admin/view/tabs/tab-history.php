cde294239c77cf7c7ac2a5cdb2f702524ea25f58
zekinah <zjlecaros@gmail.com>
2020-09-14 19:08:14 -0700

    Fixed initial load or data

--- admin/view/tabs/tab-history.php
+++ admin/view/tabs/tab-history.php
@@ -26,16 +26,18 @@
         </thead>
         <tbody id="body_links">
             <?php
-            foreach($tbl_visits as $visitsID => $row) {
-            ?>
-            <tr id="link-<?= $row->RedirectVisit_ID ?>">
-                <td><?= $row->RedirectVisit_ID ?></td>
-                <td><?= $row->Visited_From ?></td>
-                <td><?= $row->Visited_To ?></td>
-                <td><?= $row->Visited_Type ?></td>
-                <td><?= $row->Last_visited_Date ?></td>
-            </tr>
-            <?php
+            if (is_array($tbl_visits) || is_object($tbl_visits)) {
+                foreach($tbl_visits as $visitsID => $row) {
+                ?>
+                <tr id="link-<?= $row->RedirectVisit_ID ?>">
+                    <td><?= $row->RedirectVisit_ID ?></td>
+                    <td><?= $row->Visited_From ?></td>
+                    <td><?= $row->Visited_To ?></td>
+                    <td><?= $row->Visited_Type ?></td>
+                    <td><?= $row->Last_visited_Date ?></td>
+                </tr>
+                <?php
+                }
             }
             ?>
         </tbody>
