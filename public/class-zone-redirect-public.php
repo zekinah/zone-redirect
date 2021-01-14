4b78ce3474a8de2722b00bbb5d2e42d47b63a79d
zekinah <zjlecaros@gmail.com>
2021-01-13 19:11:09 -0700

    Code Tweaks

--- public/class-zone-redirect-public.php
+++ public/class-zone-redirect-public.php
@@ -62,28 +62,6 @@ class Zone_Redirect_Public {
 	}
 
 	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/zone-redirect-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/zone-redirect-public.js', array( 'jquery' ), $this->version, false );
-
-	}
-
-	/**
 	 * Outputs the Zone on the frontend
 	 */
 	public function deployZone() {
